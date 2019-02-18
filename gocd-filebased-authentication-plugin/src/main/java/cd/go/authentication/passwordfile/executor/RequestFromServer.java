c97a0f237ed1b8135aabbb347c6a3a1b441fa4c7
GaneshSPatil <ganeshpl@thoughtworks.com>
2019-02-18 13:31:30 +0530

    Fix search-users request

--- gocd-filebased-authentication-plugin/src/main/java/cd/go/authentication/passwordfile/executor/RequestFromServer.java
+++ gocd-filebased-authentication-plugin/src/main/java/cd/go/authentication/passwordfile/executor/RequestFromServer.java
@@ -27,7 +27,7 @@ public enum RequestFromServer {
     REQUEST_VERIFY_CONNECTION(String.join(".", Constants.REQUEST_PREFIX, Constants._AUTH_CONFIG_METADATA, "verify-connection")),
 
     REQUEST_AUTHENTICATE_USER(Constants.REQUEST_PREFIX + ".authenticate-user"),
-    REQUEST_SEARCH_USERS(Constants.REQUEST_PREFIX + ".find-users"),
+    REQUEST_SEARCH_USERS(Constants.REQUEST_PREFIX + ".search-users"),
 
     REQUEST_IS_VALID_USER(Constants.REQUEST_PREFIX + ".is-valid-user");
 
